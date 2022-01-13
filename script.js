2cb51ea4d28d6673244fd2e4d437e82d7aa86b0e
CodePiru <coding.jose@gmail.com>
2022-01-13 17:27:21 -0500

    Create playRound function

--- script.js
+++ script.js
@@ -6,4 +6,28 @@ function computerPlay() {
     return moves[Math.floor(Math.random() * 3)];
 }
 
-console.log(computerPlay());
+//Create a function that plays a single round
+function playRound(playerSelection, computerSelection) {
+    //Player to capitalize
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    //If player move is Invalid, invalid move
+    if (!moves.includes(playerSelection)) {
+        return "Invalid move";
+    }
+    //If both plays have the same move, its a tie
+    if (computerSelection === playerSelection) {
+        return "It's a tie!"
+    }
+    //If player = rock and CPU = scissors, you win
+    if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
+    //If player = paper and CPU = rock, you win
+        (playerSelection === "Paper" && computerSelection === "Rock") ||
+    //If player = scissors and CPU = paper, you win
+        (playerSelection === "Scissors" && computerSelection === "Paper")) {
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+    //Else, you lose
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
